b62674bf646c4d3a66be955d3da78564f531750c
sggv950 <38014323+sggv950@users.noreply.github.com>
2021-08-17 17:29:16 +0300

    commit server

--- index.js
+++ index.js
@@ -4,12 +4,7 @@ const cors = require('cors');
 const app = express();
 const http = require('http').createServer(app);
 const path = require('path');
-const io = require('socket.io')(http, {
-  cors: {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'DELETE'],
-  },
-});
+const io = require('socket.io')(http);
 const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
